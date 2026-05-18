4f0dd59946b8a4b9d38a9d2727c63ba6e32db8e8
zhengzhuang <zhengzhuang@yuanduyun.com>
2026-05-18 10:06:40 +0800

    chore: release v0.1.4 - 禁用右键菜单、托盘菜单宽度优化

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { I18nProvider } from "./i18n";
 
+// Disable WebView default context menu
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nProvider>
